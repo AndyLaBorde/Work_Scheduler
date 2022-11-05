dc1efe4b3b4cab5357d2e2b8827152e1096e4bb4
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2022-11-04 18:00:29 -0600

    added divs for office hours in html, and added running clock in js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentTime = dayjs();
+var currentDay = $('#currentDay')
+var hour9 = $('#hour-9');
+var hour10 = $('#hour-10');
+var hour11 = $('#hour-11');
+var hour12 = $('#hour-12');
+var hour1 = $('#hour-1');
+var hour2 = $('#hour-2');
+var hour3 = $('#hour-3');
+var hour4 = $('#hour-');
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
+  $(document).on('click', '.saveBtn', function() {
+    // DOM traversal can be used to get the "hour-x" id of the parent element a.k.a ->
+    var parentEl = $(this).parent();
+    console.log(parentEl);
+    console.log(parentEl[0].id);
+  })
+  // How might the id be
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +39,14 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  // $('#currentDay').text(currentTime.$d);
+  window.onload = displayClock();
+  function displayClock(){
+  var display = new Date().toLocaleTimeString();
+  currentDay.text(display);
+  setTimeout(displayClock, 1000); 
+  
+}
 });
+
